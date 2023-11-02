17e3e036b679bf45a737d8f195bf493461a45a9a
Hetwin <erwin.hetke@gmx.de>
2023-11-02 15:12:15 +0100

    Persönliche Ordner erstellen

---
+++ Erwin/My_Main.R
@@ -0,0 +1,14 @@
+# %%%%%%%%%%%%%%%%%%%%%%%
+#
+# My_Main-Skript
+#
+# Beschreibung: 
+# Hier werden die Daten eingelesen und anonymisiert. 
+#
+# %%%%%%%%%%%%%%%%%%%%%%%
+
+
+library(sdcMicro)
+# install.package("readr") # Fuer das Einlesen der Daten
+
+source(file = file.path("R", "Daten_einlesen.R"))
